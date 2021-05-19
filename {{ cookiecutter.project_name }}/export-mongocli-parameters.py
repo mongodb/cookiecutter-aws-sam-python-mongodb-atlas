2f00af065c4127a16e64133bd3264889a8b92ca3
Jason Mimick <jmimick@gmail.com>
2021-05-19 10:19:00 -0400

    change helper file

---
+++ {{ cookiecutter.project_name }}/export-mongocli-parameters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# export-mongocli-parameter.py
+# Tool to export mongocli parameters to sam aps and CloudFormation
+#
+# $source <(./export-mongocli-config.py)
+#
+import os, sys, toml
+homepath=os.path.expanduser("~")
+config="%s/.config/mongocli.toml" % homepath
+t=toml.load(config)
+if len(sys.argv)>1:
+    profile = sys.argv[1]
+else:
+    profile="default"
+if not profile in t:
+    raise Exception(f"No profile '{profile}' found in {config}")
+d=t[profile]
+if len(sys.argv)>2:
+    project_name = sys.argv[2]
+else:
+    project_name = "atlas-sam-app"
+
+print( f"ParameterKey=PublicKey,ParameterValue={d['public_api_key']} ParameterKey=PrivateKey,ParameterValue={d['private_api_key']} ParameterKey=OrgId,ParameterValue={d['org_id']} ParameterKey=ProjectName,ParameterValue={project_name}" )
+
+
