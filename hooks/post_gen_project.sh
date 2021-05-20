6a700119b4f41a4b1336810b39bb8dbb5fcf1b9b
Jason Mimick <jmimick@gmail.com>
2021-05-20 15:55:01 -0400

    simple bash post hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+cat <<- EOF
 
-echo " +-+-+-+-+-+-+-+-+-+-+-+-+-+ "
-echo " |M|o|n|g|o|D|B|-|A|t|l|a|s| "
-echo " +-+-+-+-+-+-+-+-+-+-+-+-+-+ "
-echo "Hello $(whoami)!"
-echo "Welcome to your MongoDB Atlas Project:{{ cookiecutter.project_name }}"
-echo "Next steps:"
-echo "cd {{ cookiecutter.project_name }}"
-echo "sam build"
-echo "sam deploy --guided --extra-parameters \$(./export mongocli-parameters.py)"
+ +-+-+-+-+-+-+-+-+-+-+-+-+-+
+ |M|o|n|g|o|D|B|-|A|t|l|a|s|
+ +-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+Hello $(whoami)!
+Welcome to your MongoDB Atlas Project: {{ cookiecutter.project_name }}
+Next steps: (you can cut and paste this snippet)
+
+--------------------------------------------------------------------------
+
+cd {{ cookiecutter.project_name }}
+sam build
+sam deploy --guided --parameter-overrides \$(./export-mongocli-parameters.py)
+
+--------------------------------------------------------------------------
+
+EOF
 
