c0ba1e218a8c7916aae2a5810ed869969361f6da
Jason Mimick <jmimick@gmail.com>
2021-05-21 12:20:43 -0400

    freshen up ui

--- {{ cookiecutter.project_name }}/hello_world/app.py
+++ {{ cookiecutter.project_name }}/hello_world/app.py
@@ -12,12 +12,14 @@ PASSWORD = os.environ.get('MONGODB_PASSWORD')
 PROJECT_ID = os.environ.get('MONGODB_ATLAS_PROJECT_ID')
 APP_NAME = os.environ.get('APP_NAME')
 CLUSTER_NAME = os.environ.get('MONGODB_ATLAS_CLUSTER_NAME')
+CLUSTER_ID = os.environ.get('MONGODB_ATLAS_CLUSTER_ID')
 
 print( f"DB_URI={DB_URI}")
 print( f"USERNAME={USERNAME}")
 print( f"PROJECT_ID={PROJECT_ID}")
 print( f"APP_NAME={APP_NAME}")
 print( f"CLUSTER_NAME={CLUSTER_NAME}")
+print( f"CLUSTER_ID={CLUSTER_ID}")
 client = pymongo.MongoClient( host=DB_URI,
                               username=USERNAME,
                               password=PASSWORD,
@@ -82,6 +84,7 @@ def lambda_handler(event, context):
             content = template.render(project_id=PROJECT_ID,
                                       project_name=APP_NAME,
                                       cluster_name=CLUSTER_NAME,
+                                      cluster_id=CLUSTER_ID,
                                       log_object=return_object)
         else:
             content_type = 'application/json'
